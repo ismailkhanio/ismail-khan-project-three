32232eac97bc30b3fbad09e079cd23515a52dcf5
Ismail Khan <ismailislamkhan@gmail.com>
2020-09-13 20:30:34 -0400

    style changes

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ app.addLine = function () {
   $('#addRow').click(function(){
     $('.dynamicItemRow').append (
       ` <div class="dynamicRow">
-          <input type="text" class= "fieldInput inputRow itemDescInput" maxlength="30">
-          <input type="number" class= "fieldInput inputRow amountVal" name="" id="" maxlength="6">
-        </div> 
+					<label for="itemInput"></label>
+					<input type="text" class="fieldInput inputRow itemDescInput" id="itemInput" name="itemInput" maxlength="30">
+					<div class="amountFlexContainer">
+						<p class="currencySymbol fieldInput">$</p>
+						<label for="amountVal"></label>
+						<input type="number" class="fieldInput inputRow amountVal" id="amountVal" name="amountValue" min="0" maxlength="6">
+					</div>
+				</div>
       `
     )
     app.rowPrice();
@@ -26,11 +31,11 @@ app.rowPrice = function () {
         rowPrice += parseFloat(inputVal);
       }
     });
-    console.log(rowPrice);
     $('.subtotalVal').text(`$ ${rowPrice}`);
   })
 }
 
+// initialize app
 app.init = function() {
   console.log("app initialized!");
   app.rowPrice();
